e8f9feedba61f3e28c00a1200cf90880d0e69822
Ваня Витюк <naharamimo@gmail.com>
2023-12-18 01:54:48 +0200

    Добавьте файлы проекта.

---
+++ Queue_6.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <deque>
+#include <vector>
+
+using namespace std;
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> result;
+    deque<int> dq;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        while (!dq.empty() && dq.front() < i - k + 1) {
+            dq.pop_front();
+        }
+
+        while (!dq.empty() && nums[dq.back()] < nums[i]) {
+            dq.pop_back();
+        }
+
+        dq.push_back(i);
+
+        if (i >= k - 1) {
+            result.push_back(nums[dq.front()]);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> nums1 = { 1, 3, -1, -3, 5, 3, 6, 7 };
+    int k1 = 3;
+    vector<int> result1 = maxSlidingWindow(nums1, k1);
+
+    cout << "Example 1 Output: [";
+    for (int i = 0; i < result1.size(); ++i) {
+        cout << result1[i];
+        if (i < result1.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]\n";
+
+    vector<int> nums2 = { 1 };
+    int k2 = 1;
+    vector<int> result2 = maxSlidingWindow(nums2, k2);
+
+    cout << "Example 2 Output: [";
+    for (int i = 0; i < result2.size(); ++i) {
+        cout << result2[i];
+        if (i < result2.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "]\n";
+
+    return 0;
+}
